50c9bc3e5e0900a146c18d680aebf70e89f4c550
sgmartinss <sgmartins777@gmail.com>
2025-01-08 17:13:47 -0300

    feat: Criado base do projeto para desenvolvimento futuro

---
+++ src/test/java/br/com/sgsa/system/user/security/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sgsa.system.user.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
